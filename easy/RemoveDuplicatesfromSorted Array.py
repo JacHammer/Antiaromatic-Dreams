22cb9c548a99a4f30773802ea930b300acac5c41
Minci Zhou <zmc1996@hotmail.com>
2020-03-02 03:00:11 -0700

    Fixed Solution for Remove Dups from Array

--- easy/RemoveDuplicatesfromSorted Array.py
+++ easy/RemoveDuplicatesfromSorted Array.py
@@ -3,14 +3,21 @@ from typing import List
 
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
-        x = 1
-        for i in range(len(nums) - 1):
-            if (nums[i] != nums[i + 1]):
-                nums[x] = nums[i + 1]
-                x += 1
-        return (x)
+        n_dup = 0
+        l = len(nums)
+        i = 0
+        while i < l - 1:
+            if nums[i] == nums[i + 1]:
+                nums.pop(i)
+                # i += 1
+                l -= 1
+            else:
+                i += 1
+        return l
 
 
-nums = [0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 3, 4]
 sol = Solution()
-print(sol.removeDuplicates(nums))
+nums = [1, 1, 1, 1, 2, 3, 3, 3, 4, 4, 4, 5, 5, 6, 7, 8, 8, 9]
+ans = sol.removeDuplicates(nums)
+print(ans)
+print(nums)
